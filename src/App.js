bbd6975e9030052f9856cdef9d0628e21010ebbf
Aditya Vanjari <aditya.vanjari@atyeti.com>
2023-07-25 23:21:32 +0530

    Added dark and light mode toggle for nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Navbar from "./component/Navbar";
-
+import { useState } from "react";
 
 function App() {
+  const [Theme, setTheme] = useState("light");
+
+  const changeTheme = () => {
+    if (Theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
 
   return (
-<Navbar title = "JavaScript Utils"></Navbar>
+    <Navbar
+      title="JavaScript Utils"
+      theme={Theme}
+      changeTheme={changeTheme}
+    ></Navbar>
   );
 }
 
